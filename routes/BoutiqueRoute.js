c6224ec678b20b6a6708d71a952c31d6641e4319
Miantsa2 <ranaivosonmiantsa@gmail.com>
2026-02-09 18:17:25 +0300

    [IMPL] notif is sendes when admin check event

--- routes/BoutiqueRoute.js
+++ routes/BoutiqueRoute.js
@@ -150,4 +150,64 @@ router.delete("/deleteBoutique/:id", auth(["admin"]), async (req, res) => {
   }
 });
 
+router.put("/updateNotif/:id", auth([]), async (req, res) => {
+  try {
+    const { description, titre } = req.body;
+
+    if (!description) {
+      return res.status(400).json({ message: "description is required" });
+    }
+
+    const notification = {
+      description,
+      titre: titre || 'Notification',
+      envoyeur: req.user.id || null
+      // date_envoyee est auto
+    };
+
+    const boutique = await Boutique.findByIdAndUpdate(
+      req.params.id,
+      {
+        $push: {
+          notifications: notification
+        }
+      },
+      { new: true }
+    );
+
+    if (!boutique) {
+      return res.status(404).json({ message: "cannot find store" });
+    }
+
+    res.status(200).json({
+      message: "Notification added successfully",
+      notifications: boutique.notifications
+    });
+
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+
+
+router.put('/readNotif/:id/notifications/lue', async (req, res) => {
+    try {
+        const result = await Boutique.updateOne(
+            { _id: req.params.id },
+            // $[].est_lu signifie : "Pour chaque élément du tableau, passe est_lu à true"
+            { $set: { "notifications.$[].est_lue": true } }
+        );
+
+        if (result.modifiedCount === 0) {
+            return res.status(404).json({ message: "Aucune notification à mettre à jour" });
+        }
+
+        res.status(200).json({ message: "Notifications mises à jour !" });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
+
 module.exports = router;
